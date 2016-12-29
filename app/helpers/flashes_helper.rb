28cd820a4ef8441a3b49fa3d2aa71d553be92177
Johnson Liang <johnson.liangxinzhong@gmail.com>
2016-12-29 16:38:29 +0800

    revise flashes_helper extension name

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,8 +1,8 @@
-moduel FlashesHelper
+module FlashesHelper
   FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning" }.freeze
 
   def flash_class(key)
-    FLASH_CLASSES.fetch key.to.sym, key
+    FLASH_CLASSES.fetch key.to_sym, key
   end
 
   def user_facing_flashes
